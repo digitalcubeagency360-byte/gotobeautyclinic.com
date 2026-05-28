0e764948720c96e5daa9ee131e4a7b40490a900b
digitalcubeagency360-byte <digitalcubeagency360@gmail.com>
2026-05-29 03:44:24 +0800

    Revert "Reapply "fix: polyfill CustomEvent for CI build""
    This reverts commit 572ad58deda665ef9240863f65d85f841aeb7999.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-﻿import './polyfills/custom-event.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+﻿import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -11,4 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
