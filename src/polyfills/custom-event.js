572ad58deda665ef9240863f65d85f841aeb7999
digitalcubeagency360-byte <digitalcubeagency360@gmail.com>
2026-05-29 03:43:03 +0800

    Reapply "fix: polyfill CustomEvent for CI build"
    This reverts commit 352286bf8c6b7aa1f653b3daf61c9b8e047f8f00.

---
+++ src/polyfills/custom-event.js
@@ -0,0 +1,11 @@
+﻿if (typeof globalThis.CustomEvent === 'undefined') {
+  class CustomEventPolyfill {
+    constructor(type, params = {}) {
+      this.type = type;
+      this.detail = params.detail === undefined ? null : params.detail;
+      this.bubbles = !!params.bubbles;
+      this.cancelable = !!params.cancelable;
+    }
+  }
+  globalThis.CustomEvent = CustomEventPolyfill;
+}
